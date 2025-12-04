25090c2bf1eb4962703c97df7eb3758623732c5c
Magesh K <hackmagesh1@gmail.com>
2025-12-04 08:54:55 +0000

    changes

--- app.py
+++ app.py
@@ -271,5 +271,5 @@ def init_db():
             db.session.commit()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, port=3000)
+    app.run(debug=True, use_reloader=False, port=5000)
 
